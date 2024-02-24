35f349bfec9ef309090e4916c4f16a960510feb2
Tooba Aftab <142301970+Tooba3125@users.noreply.github.com>
2024-02-24 18:02:39 +0500

    Create install color package.js

---
+++ install color package.js
@@ -0,0 +1,9 @@
+//As you want to install an external package
+//For example I want to download THE EXTERNAL Package of COLOURS so,
+//In terminal direct write "npm i colors"
+//As i mentioned package.json file you see in the last named as "Description" how many package are being externally installed and with their name.
+//For example you will see like that:
+// dependencies": {
+//     "colors": "^1.4.0"
+//   }
+//and you will see a file aumatically created named as "node-modules", with contains packages.
