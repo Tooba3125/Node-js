e3cafb19a2cc1f8050ff5a530c53316f3b69a89c
Tooba Aftab <142301970+Tooba3125@users.noreply.github.com>
2024-02-24 17:51:16 +0500

    Create result of package.json.js

---
+++ result of package.json.js
@@ -0,0 +1,20 @@
+{
+  "name": "meme",
+  "version": "1.0.0",
+  "description": "code step by step",
+  "main": "app.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "(https://github.com/Tooba3125/note-js-bug-free-tribble-.git)"
+  },
+  "keywords": [
+    "code",
+    "with",
+    "me"
+  ],
+  "author": "Tooba Aftab",
+  "license": "ISC"
+}
