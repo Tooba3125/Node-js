bd5589a2462fe4ceb099729566cf55a5efe13637
Tooba Aftab <142301970+Tooba3125@users.noreply.github.com>
2024-02-24 17:49:46 +0500

    Create package.json.js

---
+++ package.json.js
@@ -0,0 +1,45 @@
+// PS C:\Users\Tesla Laptops\Desktop\filename> npm init
+// This utility will walk you through creating a package.json file.
+// It only covers the most common items, and tries to guess sensible defaults.
+
+// See `npm help init` for definitive documentation on these fields
+// and exactly what they do.
+
+// Use `npm install <pkg>` afterwards to install a package and    
+// save it as a dependency in the package.json file.
+
+// Press ^C at any time to quit.
+// package name: (meme)
+// version: (1.0.0)
+// description: code step by step
+// entry point: (app.js)
+// test command:
+// git repository: (https://github.com/Tooba3125/note-js-bug-free-tribble-.git)
+// keywords: code with me
+// author: Tooba Aftab
+// license: (ISC)
+// About to write to C:\Users\Tesla Laptops\Desktop\meme\package.json:
+
+// {
+//   "name": "meme",
+//   "version": "1.0.0",
+//   "description": "code step by step",
+//   "main": "app.js",
+//   "scripts": {
+//     "test": "echo \"Error: no test specified\" && exit 1"      
+//   },
+//   "repository": {
+//     "type": "git",
+//     "url": "(https://github.com/Tooba3125/note-js-bug-free-tribble-.git)"
+//   },
+//   "keywords": [
+//     "code",
+//     "with",
+//     "me"
+//   ],
+//   "author": "Tooba Aftab",
+//   "license": "ISC"
+// }
+
+
+// Is this OK? (yes)
